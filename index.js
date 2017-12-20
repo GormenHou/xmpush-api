a47c40166b6b8f6c2b3b0dc16efb970b54043bef
gormenhou@GormenHous-MacBook-Pro.local <gormen@boqii.com>
2017-12-20 21:16:28 +0800

    添加index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+var SendUserAccountMessage = require('../lib/sendUserAccountMessage');
+var SendAliasMessage = require('../lib/sendAliasMessage');
+var SendTopicMessage = require('../lib/sendTopicMessage');
+
+module.exports = {
+    SendUserAccountMessage: SendUserAccountMessage,
+    SendAliasMessage: SendAliasMessage,
+    SendTopicMessage: SendTopicMessage
+}
